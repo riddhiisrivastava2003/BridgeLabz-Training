280e9ac047bec6bee955171056a5d1ca4e1c69cd
Riddhi Srivastava <riddhisrivastava2003@gmail.com>
2025-12-19 15:16:29 +0530

    SmallestOfThree

---
+++ core-java-practice/gcr-codebase/control-flow/FirstSmallestNumber.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class FirstIsSmallest{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int number1=sc.nextInt(), number2=sc.nextInt(),number3=sc.nextInt();
+        if(number1<number2 && number1<number3){
+            System.out.println("Is the first number smallest? Yes");
+        }
+        else{
+             System.out.println("Is the first number smallest? Yes");
+        }
+    }
+}
