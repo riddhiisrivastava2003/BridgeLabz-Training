32750ff17e6599e48bc3ef344b433e0574c88c29
riddhiisrivastava2003 <riddhisrivastava922@gmail.com>
2025-12-19 11:02:35 +0530

    Refactored

--- core-java-practice/gcr-codebase/programming-elements/VolumeOfEarth.java
+++ core-java-practice/gcr-codebase/programming-elements/VolumeOfEarth.java
@@ -3,13 +3,13 @@ public class VoulmeOfEarth{
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
         double pi=3.14;
-        double radiuskm =6837;
-        double volumekm=(4.0/3.0)*pi*radiuskm*radiuskm*radiuskm;
-        double radiusMiles=radiuskm*0.621371;
+        double radiusKm =6837;
+        double volumeKm=(4.0/3.0)*pi*radiusKm*radiusKm*radiusKm;
+        double radiusMiles=radiusKm*0.621371;
         double volumeMiles=(4.0/3.0)*pi*radiusMiles*radiusMiles*radiusMiles;
         
         
-        System.out.println("The voulme of Earh in cubic kilometers is "+volumekm+"\n"+
+        System.out.println("The voulme of Earh in cubic kilometers is "+volumeKm+"\n"+
               "The volume of earth in cubic miles is "+volumeMiles);
 
 
