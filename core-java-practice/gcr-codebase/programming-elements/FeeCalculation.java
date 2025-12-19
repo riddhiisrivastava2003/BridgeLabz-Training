40613a822039b6e8916cc486156ba8ae447e7556
riddhiisrivastava2003 <riddhisrivastava922@gmail.com>
2025-12-19 11:08:18 +0530

    Refactored

--- core-java-practice/gcr-codebase/programming-elements/FeeCalculation.java
+++ core-java-practice/gcr-codebase/programming-elements/FeeCalculation.java
@@ -4,8 +4,8 @@ public class FeeCalculation{
         Scanner sc=new Scanner(System.in);
         int fee=125000;
         int discountPercent=10;
-        double discountamt= (discountPercent/100.0)*fee;
-        double discountedfee=fee-discountamt;
-        System.out.println("The discount amount is INR+ "+discountamt+" and the discounted fee is INR "+discountedfee);
+        double discountAmt= (discountPercent/100.0)*fee;
+        double discountedfee=fee-discountAmt;
+        System.out.println("The discount amount is INR+ "+discountAmt+" and the discounted fee is INR "+discountedFee);
     }
 }
