98dad1ecb4900247d3cb8262b37d6a89e1397947
Riddhi Srivastava <riddhisrivastava2003@gmail.com>
2025-12-19 19:59:28 +0530

    [Riddhi Srivastava] Add .PowerOfNumber Level 2  in contorl-flow

---
+++ core-java-practice/gcr-codebase/control-flow/powerOfNumber.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class PowerOfNumber{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+
+        int number=sc.nextInt();
+        int power =sc.nextInt();
+
+        if(number <=0 || power <0){
+            System.out.println("Invalid Input");
+        }
+
+        else{
+            int result=1;
+
+            for(int i=1;i<=power;i++){
+                result=result*number;
+
+            }
+
+            System.out.println("result " +result);
+        }
+    }
+}
