707cf8e1199260dbf0700886142de18a8baba146
Riddhi Srivastava <riddhisrivastava2003@gmail.com>
2025-12-19 19:17:51 +0530

    [Riddhi Srivastava] Add .FizzBuzzUsingWhile Level 2  in contorl-flow

---
+++ core-java-practice/gcr-codebase/control-flow/FizzBuzzUsingWhile.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class FizzBuzzUsingWhile{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+
+        int number=sc.nextInt();
+        int i=1;
+        if(number<0) System.out.println("Invalid input");
+        else{
+            while(i<=number){
+                if(i%3==0 && i%5==0) System.out.println("FizzBuzz");
+               else if(i%3==0) {System.out.println("Fizz");}
+                else if(i%5==0){System.out.println("Buzz");}
+                
+                    else System.out.println(i);
+                    i++;
+            }
+        }
+    }
+}
