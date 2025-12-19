1a1eae8a27f5a7fbfdafab07f6ee836ae06966fb
riddhiisrivastava2003 <riddhisrivastava922@gmail.com>
2025-12-19 11:03:17 +0530

    Refactored

--- core-java-practice/gcr-codebase/programming-elements/temperature.java
+++ core-java-practice/gcr-codebase/programming-elements/temperature.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-class temperature{
+class Temperature{
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
         double temp=sc.nextDouble();
