7516d08b48df528dbfb09f00f48bf9edf9e4d21f
Riddhi Srivastava <riddhisrivastava2003@gmail.com>
2025-12-20 18:03:12 +0530

    [Riddhi Srivastava] Add .Multiplication Table From Six To Nine in array-practices

---
+++ core-java-practice/gcr-codebase/array-practices/MultiplicationTableSixToNine.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class MultiplicationTableSixToNine{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("Enter a number");
+        int num=sc.nextInt();
+
+        int[] result=new int[4];
+
+        int index=0;
+        for(int i=6;i<=9;i++){
+            result[index]=num*i;
+            index++;
+        }
+
+        index=0;
+        for(int i=6;i<=9;i++){
+            System.out.println(num+" * "+i+" = "+result[index]);
+            index++;
+        }
+       
+        
+    }
+}
