bb9b4999720e3640dd226490005046da42865c34
Riddhi Srivastava <riddhisrivastava2003@gmail.com>
2025-12-26 19:28:43 +0530

    [Riddhi Srivastava] Add .Scenario Based Festival Lucky Draw

---
+++ core-java-practice/scenario-based/FestivalLuckyDraw.java
@@ -0,0 +1,36 @@
+import java.util.*;
+public class FestivalLuckyDraw{
+
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("enter the number of visitors");
+        int visitors=sc.nextInt();
+
+        for(int i=1;i<=visitors;i++){
+
+            System.out.println("visitor "+i+"enter your number");
+            (!sc.hasNextInt()){
+                System.out.println("invalid input...enter valid number");
+                sc.next();
+                continue;
+            }
+
+            int number=sc.nextInt();
+            if(number<=0){
+                System.out.println("the number must be positive");
+
+
+            }
+
+            if(number %3==0 && number %5==0){
+                System.out.println("Congratulations!!!...you won a gift..");
+            }
+            else{
+                System.out.println("better luck next time!");
+            }
+
+        }
+    }
+
+}
