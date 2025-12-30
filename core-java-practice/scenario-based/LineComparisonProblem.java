5975b1441b12643b27e8a825093c5aea13f0ec73
Riddhi Srivastava <riddhisrivastava2003@gmail.com>
2025-12-30 11:53:37 +0530

    [Riddhi Srivastava] Add .LineComparisonProblem

---
+++ core-java-practice/scenario-based/LineComparisonProblem.java
@@ -0,0 +1,101 @@
+
+
+import java.util.*;
+
+
+
+public class LineComparisonProblem {
+	
+	public static double cartesianSystem(int x1,int y1,int x2,int y2) {
+		
+		return Math.sqrt(Math.pow(x2-x1,2)+Math.pow(y2-y1,2));
+		
+	}
+
+	
+	
+	public static void equalityCheck(Double length1,Double length2) {
+		System.out.println("Use case 2: checking if the lines are equal");
+		if(length1.equals(length2)) {
+			
+			
+			System.out.println("Both lines are equal");
+			
+		}
+		else {
+			System.out.println("The lines are not equal");
+		}
+	}
+	
+	public static void comparingLines(Double length1, Double length2) {
+		int result=length1.compareTo(length2);
+		System.out.println("Use case 3: comparing two lines ");
+		
+		if(result==0) System.out.println("both lines are equal");
+		else if (result>0) System.out.println("line 1 is greater than line 2");
+		else System.out.println("line 2 is greater than line 1");
+		
+		
+		
+	}
+	
+	
+	public static void main(String args[]) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the coordinates int x1,y1,x2,y2 format");
+		int x1=sc.nextInt();
+		int y1=sc.nextInt();
+		int x2=sc.nextInt();
+		int y2=sc.nextInt();
+		System.out.println("enter the coordinates int x3,y3,x4,y4 format");
+		int x3=sc.nextInt();
+		int y3=sc.nextInt();
+		int x4=sc.nextInt();
+		int y4=sc.nextInt();
+		
+		
+		
+		Double line1length=cartesianSystem(x1,y1,x2,y2);
+		Double line2length=cartesianSystem(x3,y3,x4,y4);
+		
+		
+		System.out.println("enter choice");
+		System.out.println("1 for calculating line length");
+		System.out.println("2 for equality of lines ");
+		System.out.println("3 for comparing line length");
+		
+		
+		
+		int choice=sc.nextInt();
+		
+		switch(choice) {
+		case 1:
+			System.out.println("Use case1: cartesian system length ");
+			System.out.println("Length of line 1 "+line1length);
+			System.out.println("Length of line 2 "+line2length);
+			break;
+		case 2:
+			
+			equalityCheck(line1length,line2length);
+			break;
+		case 3:
+			comparingLines(line1length,line2length);
+			break;
+		default:
+			System.out.println("invalid choice");
+			
+			
+			
+			
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
