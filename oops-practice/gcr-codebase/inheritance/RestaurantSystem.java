51062dd7ce5f98becf44d4b93aec879c8d36fc2c
Riddhi Srivastava <riddhisrivastava2003@gmail.com>
2026-01-03 14:55:14 +0530

    inheritance

--- oops-practice/gcr-codebase/inheritance/RestaurantSystem.java
+++ oops-practice/gcr-codebase/inheritance/RestaurantSystem.java
@@ -1,24 +1,23 @@
 import java.util.*;
 
-class Person{
+class Rest{
     String name;
     int id;
- 
-    Person(String name,int id){
-        this.name = name;
-        this.id = id;
 
+   Rest(String name , int id){
+        this.name = name;
+
+        this.id = id;
     }
 
     void displayDetails(){
         System.out.println("name: " + name);
+
         System.out.println("id: " + id);
 
     }
 
-
 }
-
 
 interface Worker{
 
@@ -26,40 +25,46 @@ interface Worker{
     
 }
 
-class Chef extends Person implements  Worker{
+class Chef extends Rest implements  Worker{
+
     String speciality;
 
     public Chef(String name, int id, String speciality) {
+
         super(name, id);
 
         this.speciality = speciality;
 
     }
 
-    //implementing performduties 
+
     public void performDuties(){
-        System.out.println("role : chef");
+        System.out.println("role: chef");
         System.out.println("speciality: " + speciality);
-        System.out.println("duties: preparing and cooking meals.");
+        System.out.println("duties: preparing and cooking meals");
 
     }
 
     
 }
 
-class Waiter extends Person implements  Worker{
+class Waiter extends Rest implements  Worker{
 
     int countTable;
-    Waiter (String name, int id, int countTable){
-    super(name,id);
+
+    Waiter(String name , int id, int countTable){
+    super(name, id);
     this.countTable = countTable;
     }
 
+ 
 
     public void performDuties(){
-        System.out.println("role : waiter");
+        System.out.println("role: waiter");
+
         System.out.println("table assigned: " + countTable);
-        System.out.println("duties : serving food and assisting customers");
+
+        System.out.println("duties: serving food and assisting customers");
 
     }
 
@@ -69,34 +74,36 @@ public class RestaurantSystem {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("enter chef details ");
-        System.out.println("name : ");
+        System.out.print("name: ");
         String chefName = sc.nextLine();
 
-        System.out.println("id : ");
+        System.out.print("id: ");
         int chefId = sc.nextInt();
 
-        System.out.println("specialilty : ");
+        sc.nextLine();
+
+        System.out.print("specialilty: ");
         String speciality = sc.nextLine();
 
-        Chef chef = new Chef(chefName , chefId , speciality);
+        Chef chef = new Chef(chefName ,chefId,speciality);
 
         System.out.println("\nenter waiter details");
-        System.out.println("name : ");
+        System.out.print("name: ");
         String waiterName = sc.nextLine();
 
-        System.out.println("id : ");
+        System.out.print("id: ");
         int waiterId = sc.nextInt();
 
-        System.out.println("number of tables assigned : ");
+        System.out.print("number of tables assigned: ");
         int countTable = sc.nextInt();
 
-        Waiter waiter = new Waiter(waiterName, waiterId, countTable);
+        Waiter waiter = new Waiter(waiterName , waiterId , countTable);
 
-        System.out.println("\n chef information ");
+        System.out.println("\nCHef Information");
         chef.displayDetails();
         chef.performDuties();
 
-        System.out.println("\n waiter information ");
+        System.out.println("\nWaiter Information");
         waiter.displayDetails();
         waiter.performDuties();
 
