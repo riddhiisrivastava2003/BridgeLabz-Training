8fbfe87aa3c5243a85392962f23b72a06bc24dff
Riddhi Srivastava <riddhisrivastava2003@gmail.com>
2026-01-13 10:54:54 +0530

    [riddhi srivastava] Add .added inventory management

---
+++ oops-practice/scenario-based/inventory-management/Product.java
@@ -0,0 +1,37 @@
+public class Product{
+    private int id;
+    private String name;
+    private int quantity;
+
+
+    public Product(int id, String name, int quantity) {
+        this.id = id;
+        this.name = name;
+        this.quantity = quantity;
+    }
+
+
+    
+    public int getId() {
+        return id;
+    }
+
+    
+
+    public String getName() {
+        return name;
+    }
+
+
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+
+
+    public void updateQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+}
